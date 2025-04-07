a22405f193f78b011795b97b76c394c6936f63ad
Suryansh Srivastava <93089882+Suryansh2204@users.noreply.github.com>
2025-04-07 16:59:24 +0700

    change in terraform logic

--- mlOps/news-nuggets/main.tf
+++ mlOps/news-nuggets/main.tf
@@ -2,38 +2,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "news_bucket" {
-  bucket = "news-nuggets-bucket"
+# # S3 Bucket
+# resource "aws_s3_bucket" "news_bucket" {
+#   bucket = "news-nuggets-bucket"
 
-  tags = {
-    Name = "News Nuggets Bucket"
-  }
-}
+#   tags = {
+#     Name = "News Nuggets Bucket"
+#   }
+# }
 
 # S3 'folders' (prefixes with placeholder files)
-resource "aws_s3_object" "news_api_folder" {
-  bucket = aws_s3_bucket.news_bucket.id
-  key    = "news-api/.keep"
-  content = ""
-}
+# resource "aws_s3_object" "news_api_folder" {
+#   bucket = aws_s3_bucket.news_bucket.id
+#   key    = "news-api/.keep"
+#   content = ""
+# }
 
-resource "aws_s3_object" "news_data_folder" {
-  bucket = aws_s3_bucket.news_bucket.id
-  key    = "news-data/.keep"
-  content = ""
-}
+# resource "aws_s3_object" "news_data_folder" {
+#   bucket = aws_s3_bucket.news_bucket.id
+#   key    = "news-data/.keep"
+#   content = ""
+# }
 
-resource "aws_s3_object" "news_org_folder" {
-  bucket = aws_s3_bucket.news_bucket.id
-  key    = "news-org/.keep"
-  content = ""
-}
-resource "aws_s3_object" "scrapped_news_folder" {
-  bucket = aws_s3_bucket.news_bucket.id
-  key    = "scrapped_news/.keep"
-  content = ""
-}
+# resource "aws_s3_object" "news_org_folder" {
+#   bucket = aws_s3_bucket.news_bucket.id
+#   key    = "news-org/.keep"
+#   content = ""
+# }
+# resource "aws_s3_object" "scrapped_news_folder" {
+#   bucket = aws_s3_bucket.news_bucket.id
+#   key    = "scrapped_news/.keep"
+#   content = ""
+# }
 
 # Variables for reusability
 locals {
@@ -44,42 +44,42 @@ locals {
   layer_arn      = "arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python313-Arm64:1"
 }
 
-# Lambda Function Template
-resource "aws_lambda_function" "get_news" {
-  function_name    = "get_news"
-  filename         = "get_news.zip"
-  handler          = "get_news.lambda_handler"
-  runtime          = local.runtime
-  architectures    = [local.architecture]
-  role             = local.role_arn
-  source_code_hash = filebase64sha256("get_news.zip")
-  timeout          = local.timeout
-  layers           = [local.layer_arn]
-}
+# # Lambda Function Template
+# resource "aws_lambda_function" "get_news" {
+#   function_name    = "get_news"
+#   filename         = "get_news.zip"
+#   handler          = "get_news.lambda_handler"
+#   runtime          = local.runtime
+#   architectures    = [local.architecture]
+#   role             = local.role_arn
+#   source_code_hash = filebase64sha256("get_news.zip")
+#   timeout          = local.timeout
+#   layers           = [local.layer_arn]
+# }
 
-resource "aws_lambda_function" "get_secrets" {
-  function_name    = "get_secrets"
-  filename         = "get_secrets.zip"
-  handler          = "get_secrets.lambda_handler"
-  runtime          = local.runtime
-  architectures    = [local.architecture]
-  role             = local.role_arn
-  source_code_hash = filebase64sha256("get_secrets.zip")
-  timeout          = local.timeout
-  layers           = [local.layer_arn]
-}
+# resource "aws_lambda_function" "get_secrets" {
+#   function_name    = "get_secrets"
+#   filename         = "get_secrets.zip"
+#   handler          = "get_secrets.lambda_handler"
+#   runtime          = local.runtime
+#   architectures    = [local.architecture]
+#   role             = local.role_arn
+#   source_code_hash = filebase64sha256("get_secrets.zip")
+#   timeout          = local.timeout
+#   layers           = [local.layer_arn]
+# }
 
-resource "aws_lambda_function" "summarize_and_categorize" {
-  function_name    = "summarize_and_categorize"
-  filename         = "summarize_and_categorize.zip"
-  handler          = "summarize_and_categorize.lambda_handler"
-  runtime          = local.runtime
-  architectures    = [local.architecture]
-  role             = local.role_arn
-  source_code_hash = filebase64sha256("summarize_and_categorize.zip")
-  timeout          = local.timeout
-  layers           = [local.layer_arn]
-}
+# resource "aws_lambda_function" "summarize_and_categorize" {
+#   function_name    = "summarize_and_categorize"
+#   filename         = "summarize_and_categorize.zip"
+#   handler          = "summarize_and_categorize.lambda_handler"
+#   runtime          = local.runtime
+#   architectures    = [local.architecture]
+#   role             = local.role_arn
+#   source_code_hash = filebase64sha256("summarize_and_categorize.zip")
+#   timeout          = local.timeout
+#   layers           = [local.layer_arn]
+# }
 resource "aws_lambda_function" "news_scraper" {
   function_name    = "news_scraper"
   filename         = "news_scraper.zip"
